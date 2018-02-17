7e9f4e60d7ef00fc30aeadb4175bfa30ed540ae1
victor <victor_souzas@yahoo.com.br>
2018-02-16 23:05:00 -0200

    Huge refactor nedeed

--- DigitalOceanRobot.py
+++ DigitalOceanRobot.py
@@ -2,7 +2,7 @@
 
 import requests
 from lxml import html
-from Machinne import Machine
+from Htmlhandler import Htmlhandler
 
 
 class DigitalOceanRobot:
@@ -11,35 +11,32 @@ class DigitalOceanRobot:
         self.url = "https://www.digitalocean.com/pricing/"
         self.headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"}
         self.content = requests.get(url=self.url, headers=self.headers, timeout=2.5)
+        self.html_handler = Htmlhandler()
 
-    def __split_data(self, content, pattern, limit=False):
-        table = []
-
-        for i in content.xpath(pattern):
-            table.append(html.tostring(i))
-        return table
-
-    def __extract_data(self, data, pattern):
-
-        processed_data = []
-        for i in data.xpath(pattern):
-            processed_data.append(i)
-        return processed_data
+    def get_table(self, table):
+        attr = self.html_handler.extract_data(table, '//div[@class="bui-Col bui-Col-6@large"]//table//tr//th')
+        tble = self.html_handler.extract_data(table, '//div[@class="bui-Col bui-Col-6@large"]//table//tr/td')
+        self.lst = self.html_handler.split_count(tble, len(attr))
+        data = []
+        for i in self.lst:
+            data.append(dict(zip(attr, i)))
+        return data
 
     def parse(self):
         # remove javascript garbage
         content = self.content.text.split("id=tab-droplets data-bui-tabbed-nav-pane>")
         # takes what's important
         content = html.fromstring(content[1])
-        machines = self.__split_data(content, '//div[@class="bui-Col bui-Col-6@large"]')
+        machines = self.html_handler.split_data(content, '//div[@class="bui-Col bui-Col-6@large"]')
+        machines.extend(self.html_handler.split_data(html.fromstring(machines[1]), '//div[@class="bui-Col bui-Col-6@large"]//div[@class="bui-u-mb--xlarge"]'))
+        print(machines)
+        # data = {}
+        # for x in machines:
+        #     table = html.fromstring(x)
+        #     name = table.xpath('//div[@class="bui-Col bui-Col-6@large"]//h2')
+        #     data[name[0].text_content()] = self.get_table(table)
+        # print(data)
 
-        machines += self.__split_data(html.fromstring(machines[1]), '//div[@class="bui-Col bui-Col-6@large"]')
-        del machines[1]
-        table = html.fromstring(machines[0])
-        attributes = self.__extract_data(table, '//div[@class="bui-Col bui-Col-6@large"]//table//tr//th')
-
-        table_lines = self.__split_data(table, '//div[@class="bui-Col bui-Col-6@large"]//table//tr//td')
-        print(table_lines)
 
 
 if __name__ == '__main__':
