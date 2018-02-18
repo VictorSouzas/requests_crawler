b07ba65f0ee89fbf783b067353c5741dc014e41e
victor <victor_souzas@yahoo.com.br>
2018-02-18 11:58:40 -0300

    Just realized that split_data function the same way for all Crawlers

--- Robot.py
+++ Robot.py
@@ -10,3 +10,9 @@ class Robot:
         self.url = url
         self.headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"}
         self.content = requests.get(url=self.url, headers=self.headers, timeout=2.5)
+
+    def split_data(self, content, pattern):
+        table = []
+        for i, x in enumerate(content.xpath(pattern)):
+            table.append(html.tostring(x))
+        return table
